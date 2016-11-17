9ea53410a9a1fad7bbea3a093dde21e862872236
StarkShang <stark.shang@sjtu.edu.cn>
2016-11-18 01:36:48 +0800

    v2016.11.18

---
+++ Check/ver_0_0_2/Commands/Clear.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ver_0_0_2.Commands
+{
+    class Clear : ICommand
+    {
+        public void Run(string[] parameters)
+        {
+            Console.Clear();
+        }
+    }
+}
---
+++ Check/ver_0_0_2/Register.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ver_0_0_2
+{
+    class Register
+    {
+        public DirectoryInfo HomeDirectory { get; }
+        public Register(string path)
+        {
+            HomeDirectory = new DirectoryInfo(path);
+        }
+
+        public string[] SignIn()
+        {
+            var ids = from dir in HomeDirectory.GetDirectories()
+                      select dir.Name;
+            var keys = Config.NameList.Keys;
+            return ids.ToArray();
+        }
+    }
+}
